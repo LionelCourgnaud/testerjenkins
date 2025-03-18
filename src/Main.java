283d36b04a67956bd33340b7761005a3c6d08748
LionelCe <lionel.courgnaud@neuf.fr>
2025-03-18 14:03:45 +0100

    first

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import java.lang.module.Configuration;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.print ln("Salukes Monde !");
+        System.out.println("Salukes Monde !");
     }
 }
